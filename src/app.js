3c2ae58e365b556b5524a91671df2c9257069dd1
nataliey16 <102062208+nataliey16@users.noreply.github.com>
2022-06-01 11:44:18 -0600

    Updated time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
+function formatDate(timestamp) {
+  console.log(timestamp);
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let cityElement = document.querySelector("#shown-city");
@@ -12,6 +37,9 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#shown-wind");
   let wind = Math.round(response.data.wind.speed);
   windElement.innerHTML = `Wind: ${wind} km/hr`;
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "abc1ee724ce94bd135a062b94b61de5a";
